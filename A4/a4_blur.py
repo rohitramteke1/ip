917afa9c6147ab7d9efb1fcf801310cdd615be59
Rohit Maroti Ramteke <ramteker284@gmail.com>
2023-12-25 03:13:44 +0530

    edit

--- A4/a4_blur.py
+++ A4/a4_blur.py
@@ -1,30 +1,18 @@
-
 import cv2
 import numpy as np
 
-	
-# Read the image
-img = cv2.imread('A4.png', 0)
-
-# Obtain number of rows and columns
-# of the image
+img = cv2.imread(r'A4\A4.png', 0)
 m, n = img.shape
-
-# Develop Averaging filter(3, 3) mask
-mask = np.ones([3, 3], dtype = int)
-mask = mask / 9
-
-# Convolve the 3X3 mask over the image
-img_new = np.zeros([m, n])
+mask = np.ones([3, 3], dtype=int) / 9
+img_new = np.zeros([m, n], dtype=np.uint8)
 
 for i in range(1, m-1):
-	for j in range(1, n-1):
-		temp = img[i-1, j-1]*mask[0, 0]+img[i-1, j]*mask[0, 1]+img[i-1, j + 1]*mask[0, 2]+img[i, j-1]*mask[1, 0]+ img[i, j]*mask[1, 1]+img[i, j + 1]*mask[1, 2]+img[i + 1, j-1]*mask[2, 0]+img[i + 1, j]*mask[2, 1]+img[i + 1, j + 1]*mask[2, 2]
-		
-		img_new[i, j]= temp
-		
-img_new = img_new.astype(np.uint8)
+    for j in range(1, n-1):
+        temp = (img[i-1:i+2, j-1:j+2] * mask).sum()
+        img_new[i, j] = temp.astype(np.uint8)
+
 cv2.imwrite('blurred.png', img_new)
-cv2.imshow('Blurred Image',img_new)
+cv2.imshow('Original Image', img)
+cv2.imshow('Blurred Image', img_new)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
